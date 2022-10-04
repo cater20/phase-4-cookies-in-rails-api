7be6a26e4d80b3655d93497b68048d82ef4f6366
chrisphine <onyangoketta@gmail.com>
2022-10-04 13:50:42 +0300

    commit tree d578f836ee978c8eb9a7e33124e74177f57f92b9

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,8 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Phase4CookiesInRailsApi
+#module Phase4CookiesInRailsApi
+  module MyApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
